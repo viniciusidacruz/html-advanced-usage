d5017fd5f5d9c18492d3829caf91a9c4356389a9
ViniciusIDaCruz <viniciusidacruz@hotmail.com>
2025-11-30 03:07:46 -0300

    feat: build and implement componente button

---
+++ components/Button.tsx
@@ -0,0 +1,7 @@
+import { ComponentProps } from "react";
+
+export const Button = ({ children }: ComponentProps<"button">) => (
+  <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-3 rounded-lg transition-colors cursor-pointer">
+    {children}
+  </button>
+);
